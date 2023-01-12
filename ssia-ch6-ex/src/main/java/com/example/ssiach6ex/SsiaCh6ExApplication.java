0953910b237872e1f340b0579f1ba4063b735d74
shinmj <gkdlqkek@gmail.com>
2023-01-12 13:17:21 +0900

    first commit

---
+++ ssia-ch6-ex/src/main/java/com/example/ssiach6ex/SsiaCh6ExApplication.java
@@ -0,0 +1,13 @@
+package com.example.ssiach6ex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsiaCh6ExApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsiaCh6ExApplication.class, args);
+    }
+
+}
