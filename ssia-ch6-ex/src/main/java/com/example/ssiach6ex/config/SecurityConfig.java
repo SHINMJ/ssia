9c7f33d7e92c8ddbdd1f6d9bd07369fee78a0a57
shinmj <gkdlqkek@gmail.com>
2023-01-12 13:51:08 +0900

    feat: 1.암호인코더 객체 정의

---
+++ ssia-ch6-ex/src/main/java/com/example/ssiach6ex/config/SecurityConfig.java
@@ -0,0 +1,15 @@
+package com.example.ssiach6ex.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
